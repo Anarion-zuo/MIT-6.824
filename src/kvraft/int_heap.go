14b914d9da10151c6e29e260696843de69bba763
anarion <anarionzuo@outlook.com>
2022-02-19 16:27:51 +0800

    lab3a implementation before persistance

---
+++ src/kvraft/int_heap.go
@@ -0,0 +1,26 @@
+package kvraft
+
+// An IntMinHeap is a min-heap of ints.
+type IntMinHeap []int
+
+func (h IntMinHeap) Len() int           { return len(h) }
+func (h IntMinHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntMinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntMinHeap) Push(x interface{}) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.(int))
+}
+
+func (h *IntMinHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func (h *IntMinHeap) Peek() interface{} {
+	return (*h)[len(*h)-1]
+}
